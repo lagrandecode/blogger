9efe4d7149c317ddbf42ab14cadbf99a9b076331
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-01 16:48:41 +0100

    "commit"

--- detail/models.py
+++ detail/models.py
@@ -1,3 +1,11 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+
+class Detail(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    def __str__(self):
+        return self.title
