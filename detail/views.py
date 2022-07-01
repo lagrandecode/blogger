71d3e6c3223e1ae0c11088623bef911ba05300f3
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-01 16:56:41 +0100

    "commit"

--- detail/views.py
+++ detail/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
+from detail.models import Detail
 
 # Create your views here.
+
+def index(request):
+    detail = Detail.objects.all()
+    context = {
+        "detail" : detail
+    }
+    return render(request, "index.html", context)
