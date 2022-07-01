24197e23d9a6a8026147c5c1c9ce7e5d08f452a7
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-01 16:49:07 +0100

    "commit"

--- detail/models.py
+++ detail/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Detail(models.Model):
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
     def __str__(self):
         return self.title
